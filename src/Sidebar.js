348af1be48adac572be12f38621c22989871458e
buddha <buddhatamang1000@gmail.com>
2021-04-29 17:29:03 +0545

    building sidebar

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,7 +1,19 @@
 import React from "react";
 import "./Sidebar.css";
-import { Button } from "@material-ui/core";
+import { Button, IconButton } from "@material-ui/core";
 import AddIcon from "@material-ui/icons/Add";
+import InboxIcon from "@material-ui/icons/Inbox";
+import StarIcon from "@material-ui/icons/Star";
+import WatchLaterIcon from "@material-ui/icons/WatchLater";
+import SendIcon from "@material-ui/icons/Send";
+import InsertDriveFileIcon from "@material-ui/icons/InsertDriveFile";
+import LabelImportantIcon from "@material-ui/icons/LabelImportant";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import PersonIcon from "@material-ui/icons/Person";
+import DuoIcon from "@material-ui/icons/Duo";
+import PhoneIcon from "@material-ui/icons/Phone";
+
+import SidebarOption from "./SidebarOption";
 
 function Sidebar() {
   return (
@@ -12,6 +24,34 @@ function Sidebar() {
       >
         Compose
       </Button>
+
+      <SidebarOption
+        Icon={InboxIcon}
+        title="Inbox"
+        number={30}
+        selected={true}
+      />
+
+      <SidebarOption Icon={StarIcon} title="Starred" number={30} />
+      <SidebarOption Icon={WatchLaterIcon} title="Snoozed" number={54} />
+      <SidebarOption Icon={LabelImportantIcon} title="Important" number={54} />
+      <SidebarOption Icon={SendIcon} title="Sent" number={54} />
+      <SidebarOption Icon={InsertDriveFileIcon} title="Draft" number={54} />
+      <SidebarOption Icon={ExpandMoreIcon} title="More" number={54} />
+
+      <div className="sidebar__footer">
+        <div className="sidebar__footerIcon">
+          <IconButton>
+            <PersonIcon />
+          </IconButton>
+          <IconButton>
+            <DuoIcon />
+          </IconButton>
+          <IconButton>
+            <PhoneIcon />
+          </IconButton>
+        </div>
+      </div>
     </div>
   );
 }
