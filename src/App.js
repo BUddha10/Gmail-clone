d8c4fd532dae02395f4549c3d2b1e88206456f82
buddha <buddhatamang1000@gmail.com>
2021-04-29 14:05:50 +0545

    building compose button

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import { Counter } from "./features/counter/Counter";
 import "./App.css";
 
 import Header from "./Header";
+import Sidebar from "./Sidebar";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Sidebar />
     </div>
   );
 }
