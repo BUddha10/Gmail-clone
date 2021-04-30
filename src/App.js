5bd06025091f9488ec8ca3e5b15822aaad08d9aa
buddha <buddhatamang1000@gmail.com>
2021-04-30 11:30:42 +0545

    building emailList header part

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
 import React from "react";
-import { Counter } from "./features/counter/Counter";
+//import { Counter } from "./features/counter/Counter";
 import "./App.css";
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import Header from "./Header";
 import Sidebar from "./Sidebar";
+import EmailList from "./EmailList";
+import Mail from "./Mail";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Sidebar />
-    </div>
+    <Router>
+      <div className="app">
+        <Header />
+        <div className="app__body">
+          <Sidebar />
+
+          <Switch>
+            <Route path="/mail">
+              <Mail />
+            </Route>
+            <Route path="/">
+              <EmailList />
+            </Route>
+          </Switch>
+        </div>
+      </div>
+    </Router>
   );
 }
 
